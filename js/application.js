22547016eca0b9eba4d087b764605452fbbd16dd
Tyler <tylerxwebber@gmail.com>
2021-03-08 21:47:21 -0600

    Rough Draft 1.0 (minor changes and JS vars defined)

---
+++ js/application.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var product = document.getElementById('product');
+    var shoppingCart = document.getElementById('shopping-cart');
+    var itemPrice = document.getElementById('item-price'); 
+    var createButton = document.getElementById('create')
+    var totalPrice = document.getElementById('total-price')
+    var itemsTotalPrice = document.getElementById('items-total-price');
+    var removeButton = document.getElementById('remove-button')
+    var quantity = document.getElementById('quantity')
+}) 
